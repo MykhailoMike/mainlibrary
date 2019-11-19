c38b7844ef0811ca49b8f34c82f7f8ca64a0c1dc
RomanTereshchenko <roman.a.tereshchenko@gmail.com>
2019-11-19 16:14:48 +0200

    -

--- libraryFX/src/main/java/libraryFXclient/SignUpWindowController.java
+++ libraryFX/src/main/java/libraryFXclient/SignUpWindowController.java
@@ -1,13 +1,27 @@
 package libraryFXclient;
 
+import java.awt.*;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import repository.ReaderRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class SignUpWindowController {
@@ -42,15 +56,55 @@ public class SignUpWindowController {
 
     @FXML
     void initialize() {
-        ReaderRepository readerRepository = new ReaderRepository();
 
         signUpButton.setOnAction(event -> {
-            readerRepository.signUpReader(signUpFirstName.getText(), signUpLastName.getText(),
-                    signUpPhone.getText(), signUpAddress.getText(),
-                    signUpLogin.getText(), signUpPassword.getText());
-        });
 
+            System.out.println(signUpFirstName.getText());
+            System.out.println(signUpLastName.getText());
+            System.out.println(signUpPhone.getText());
+            System.out.println(signUpAddress.getText());
+            System.out.println(signUpLogin.getText());
+            System.out.println(signUpPassword.getText());
+
+        signUpButton.getScene().getWindow().hide();
+
+        final String addReaderUrl = "http://localhost:8081/users/add";
+
+        RestTemplate newReaderTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> newReaderParamsMap = new HashMap<>();
+        newReaderParamsMap.put("firstName", signUpFirstName.getText());
+        newReaderParamsMap.put("lastName", signUpLastName.getText());
+        newReaderParamsMap.put("phone", signUpPhone.getText());
+        newReaderParamsMap.put("address", signUpAddress);
+        newReaderParamsMap.put("login", signUpLogin);
+        newReaderParamsMap.put("password", signUpPassword);
+
+        HttpEntity<Map<String, Object>> newReaderEntity =
+                new HttpEntity<Map<String, Object>>(newReaderParamsMap, headers);
+
+        String result = newReaderTemplate.postForObject(addReaderUrl, newReaderEntity, String.class);
+
+        System.out.println(result);
+
+    });
     }
+
+
+
+//    {
+//        ReaderRepository readerRepository = new ReaderRepository();
+//
+//        signUpButton.setOnAction(event -> {
+//            readerRepository.signUpReader(signUpFirstName.getText(), signUpLastName.getText(),
+//                    signUpPhone.getText(), signUpAddress.getText(),
+//                    signUpLogin.getText(), signUpPassword.getText());
+//        });
+//
+//    }
 
 
 }
---
+++ transferTO/src/main/java/Reader.java
@@ -0,0 +1,69 @@
+public class Reader {
+
+    private String firstName;
+    private String lastName;
+    private String tel;
+    private String address;
+    private String login;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Reader{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", tel='" + tel + '\'' +
+                ", address='" + address + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
